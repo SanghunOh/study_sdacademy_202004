a5239d3af05f1756af71a2a1d2fa31330735ffcc
kim-so-hyeon <ssojja0987@gmail.com>
2020-05-13 17:22:55 +0900

    sohyeon

--- 20200513_pickle/use_kimsohyeon.py
+++ 20200513_pickle/use_kimsohyeon.py
@@ -1,18 +1,18 @@
-from packages.Class_kimsohyeon import Cal1 as C1
-from packages.Class_kimsohyeon import Cal2 as C2
-from packages.Class_kimsohyeon import Cal3 as C3
+from packages.class_kimsohyeon import Cal1 as C1
+from packages.class_kimsohyeon import Cal2 as C2
+from packages.class_kimsohyeon import Cal3 as C3
 
 val1 = C1(4)
-print("Predict Result :",val1.getPredict())
+print("Predict Result (x=4) :",val1.getPredict())
 
 val2 = C2(4)
-print("Predict Result :",val2.getPredict())
+print("Predict Result (x=4):",val2.getPredict())
 
 val3 = C3(4,6)
-print("Predict Result :",val3.getPredict())
+print("Predict Result (x=4, y=6):",val3.getPredict())
 
 
 import pickle
-pickle.dump(val1, open("./Pickle_kimsohyeon","wb"))
-val1_pkl = pickle.load(open("./Pickle_kimsohyeon","rb"))
-val1_pkl.getPredict()
+pickle.dump(val1, open("./pickle_kimsohyeon","wb"))
+val1_pkl = pickle.load(open("./pickle_kimsohyeon","rb"))
+print("Pickle import value :",val1_pkl.getPredict())
