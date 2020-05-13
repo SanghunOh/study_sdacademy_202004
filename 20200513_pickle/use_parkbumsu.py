61e9fb288cfc80614f46858cb510d7bf68607a70
Parkbhumsoo <hansuyum12@gmail.com>
2020-05-13 17:24:26 +0900

    bb

---
+++ 20200513_pickle/use_parkbumsu.py
@@ -0,0 +1,12 @@
+from class_parkbumsu import Test1 as T1
+from class_parkbumsu import Test2 as T2
+from class_parkbumsu import Test3 as T3
+
+A1 = T1(4)
+A1.getpredict()
+
+A2 = T2(3)
+A2.getpredict()
+
+A3 = T3(4,6)
+A3.getpredict()
