2e8891ded7e5b931ffbb29fc332cc337655e2962
Nukzan <yoonhi1030@gmail.com>
2020-05-13 14:49:15 +0900

    hyein_excpt4th

--- 20200513_pickle/Pickle_YoonHyein.py
+++ 20200513_pickle/Pickle_YoonHyein.py
@@ -1,5 +1,5 @@
 from Use_YoonHyein import s1
 import pickle
-pickle.dump(s1, open("./yhiS1.pkl","wb"))
-yhiS1_pkl = pickle.load(open("./yhiS1.pkl","rb"))
+pickle.dump(s1, open("./Hyein.pkl","wb"))
+yhiS1_pkl = pickle.load(open("./Hyein.pkl","rb"))
 yhiS1_pkl.getPredict()
--- 20200513_pickle/packages/Class_YoonHyein.py
+++ 20200513_pickle/packages/Class_YoonHyein.py
@@ -3,15 +3,21 @@ class first:
         self.y = x * 2
     def getPredict(self):
         print(self.y)
+    def innerPrint(self):
+        print("The y is ", self.y, " (using First function)")
 
 class second:
     def __init__(self, x1=0, x2=2):
         self.y = x1 + x2
     def getPredict(self):
         print(self.y)
+    def innerPrint(self):
+        print("The y is ", self.y, " (using Second function)")
 
 class third:
     def __init__(self, x=0):
         self.y = 2 + (x*0.5)
     def getPredict(self):
         print(self.y)
+    def innerPrint(self):
+        print("The y is ", self.y, " (using Third function)")
