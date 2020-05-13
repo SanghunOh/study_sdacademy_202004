14fd311a3b9000b937b461357e65c922391996af
jihyunseo <zihyun.seo@gmail.com>
2020-05-13 16:46:21 +0900

    SJH

--- 20200513_pickle/use_SJH.py
+++ 20200513_pickle/use_SJH.py
@@ -2,6 +2,10 @@ from classTEST import First as fst
 fst1 = fst()
 from classTEST import Second as Snd
 Snd1 = Snd()
+from classTEST import Third as Trd
+Trd1 = Trd()
+from classTEST import Third2 as Trdw
+Trdw1 = Trdw()
 
 
 import pickle
@@ -11,7 +15,19 @@ fst1_pkl = pickle.load( open( "./SJH.pkl", "rb" ) )
 print('load pickle')
 
 import pickle
-pickle.dump( Snd1, open( "./SJH.pkl", "wb" ) )
+pickle.dump( Snd1, open( "./SJH2.pkl", "wb" ) )
 print('dump pickle')
-Snd1_pkl = pickle.load( open( "./SJH.pkl", "rb" ) )
+Snd1_pkl = pickle.load( open( "./SJH2.pkl", "rb" ) )
+print('load pickle')
+
+import pickle
+pickle.dump( Trd1, open( "./SJH3.pkl", "wb" ) )
+print('dump pickle')
+Trd1_pkl = pickle.load( open( "./SJH3.pkl", "rb" ) )
+print('load pickle')
+
+import pickle
+pickle.dump( Trdw1, open( "./SJHw3.pkl", "wb" ) )
+print('dump pickle')
+Trdw1_pkl = pickle.load( open( "./SJHw3.pkl", "rb" ) )
 print('load pickle')
