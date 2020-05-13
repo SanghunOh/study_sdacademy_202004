a1f335897c6e5c2c9b376095b8335f304a9760b1
JunO-93 <dlwnsdh17@naver.com>
2020-05-13 16:06:44 +0900

    juno

--- 20200513_pickle/packages/class_LeeJunO.py
+++ 20200513_pickle/packages/class_LeeJunO.py
@@ -19,11 +19,27 @@ class T3() :
         return print("Predict Result : ", T3.z + 0.5 * self.x )         
 
 
-# class T4() :
-#     z = 2
-#     def __init__(self,x1,x2):
-#         self.x1 =x1
-#         self.x2 =x2        
-#     def getPredict(self):
-#         return print("Predict Result : ",z)
+class T4() :
+    
+    def __init__(self,x1,x2):
+        self.x1 =x1
+        self.x2 =x2        
+    def getPredict(self):
+        if(self.x1==0 or self.x1==1):
+            return print("Predict Result : ",self.x2*3)
+        elif(self.x1==2):
+            return print("Predict Result : ",self.x2*3-0.5)
+        else:
+            for i in range(2,self.x1):      
+                if(self.x1%i==0.0):
+                    return print("Predict Result : ",self.x2*3)                    
+                else:
+                    return print("Predict Result : ",self.x2*3-0.5)            
+       
+
+
+ 
+
+        
+            
     
--- 20200513_pickle/use_LeeJunO.py
+++ 20200513_pickle/use_LeeJunO.py
@@ -1,19 +1,53 @@
 from packages.class_LeeJunO import T1
 from packages.class_LeeJunO import T2  
 from packages.class_LeeJunO import T3 
+from packages.class_LeeJunO import T4 
 
+#T1 기존값
+print("------------------------")
+print("T1")
 x1 = T1(3)
 x1.getPredict()
-
+print("--------------------------")
+print("T2")
+#T2 기존값
 x2 = T2(1,3)
 x2.getPredict()
-
+print("--------------------------")
+print("T3")
+#T3 기존값
 x3 = T3(1)
 x3.getPredict()
+print("--------------------------")
+print("T4")
+# T4 기존값
+x4 = T4(0,2)
+x5 = T4(1,3)
+x6 = T4(2,4)
+x7 = T4(3,5)
+
+x4.getPredict()
+x5.getPredict()
+x6.getPredict()
+x7.getPredict()
+print("--------------------------")
+print("T4에 임의값")
+# T4 임의값
+
+x8 = T4(4,6)
+x9 = T4(5,7)
+x10 = T4(1,4)
+
+x8.getPredict()
+x9.getPredict()
+x10.getPredict()
+print("--------------------------")
+#import pickle & pickle.load
 
 import pickle
 
-pickle.dump(x2,open("./20200513_pickle/pickle_LeeJunO.pkl","wb"))
-x2_load = pickle.load(open("./20200513_pickle/pickle_LeeJunO.pkl","rb"))
-
-x2_load.getPredict()
+pickle.dump(x7,open("./20200513_pickle/pickle_LeeJunO.pkl","wb"))
+x7_load = pickle.load(open("./20200513_pickle/pickle_LeeJunO.pkl","rb"))
+print("pickle로 불러온 값")
+x7_load.getPredict()
+print("--------------------------")
