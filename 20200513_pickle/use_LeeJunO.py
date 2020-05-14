808b9ba56ba8b8d672f86f90d95966ea59b8bd76
JunO-93 <dlwnsdh17@naver.com>
2020-05-14 17:24:43 +0900

    20200514

--- 20200513_pickle/use_LeeJunO.py
+++ 20200513_pickle/use_LeeJunO.py
@@ -24,7 +24,7 @@ print("T4")
 x4 = T4(0,2)
 x5 = T4(1,3)
 x6 = T4(2,4)
-x7 = T4(3,5)
+x7 = T4(3,6)
 
 x4.getPredict()
 x5.getPredict()
@@ -46,8 +46,8 @@ print("--------------------------")
 
 import pickle
 
-pickle.dump(x7,open("./20200513_pickle/pickle_LeeJunO.pkl","wb"))
-x7_load = pickle.load(open("./20200513_pickle/pickle_LeeJunO.pkl","rb"))
+pickle.dump(x7,open("C:/Users/sundooedu/study_sdacademy_202004/20200513_pickle/pickle_LeeJunO.pkl","wb"))
+x7_load = pickle.load(open("C:/Users/sundooedu/study_sdacademy_202004/20200513_pickle/pickle_LeeJunO.pkl","rb"))
 print("pickle로 불러온 값")
 x7_load.getPredict()
 print("--------------------------")
